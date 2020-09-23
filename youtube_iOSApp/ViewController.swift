f9e48e447a7b45ea46928a754822d157cbcc603e
Роман Юн <romanyun@MacBook-Pro-Roman.local>
2020-09-23 14:19:13 +0900

    Initial xcode project cretion

---
+++ youtube_iOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube_iOSApp
+//
+//  Created by Роман Юн on 23.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
