c2bf2d3beba0833a8ad2bb456f76ba37e7f12138
Роман Юн <romanyun@MacBook-Pro-Roman.local>
2020-09-24 09:44:39 +0900

    Modified gitignore

--- youtube_iOSApp/ConstantsTemplate.swift
+++ youtube_iOSApp/ConstantsTemplate.swift
@@ -1,16 +1,18 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube_iOSApp
 //
-//  Created by Роман Юн on 23.09.2020.
+//  Created by Роман Юн on 24.09.2020.
 //
 
 import Foundation
 
+/*
 struct Constants {
     
-    static var API_KEY = "AIzaSyB2uRehvQaxYn9BQZcssAiQG9vrEzw-lXA"
-    static var PLAYLIST_ID = "UUt7sv-NKh44rHAEb-qCCxvA"
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
 }
+ */
