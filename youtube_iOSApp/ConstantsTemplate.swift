4024d999741aeaa3dd39c21ab3a682d544d5dea9
Роман Юн <romanyun@MacBook-Pro-Roman.local>
2020-09-25 13:37:33 +0900

    Test push Constans

--- youtube_iOSApp/ConstantsTemplate.swift
+++ youtube_iOSApp/ConstantsTemplate.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/*
+
 struct Constants {
     
     static var API_KEY = ""
@@ -15,4 +15,4 @@ struct Constants {
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
 }
- */
+ 
