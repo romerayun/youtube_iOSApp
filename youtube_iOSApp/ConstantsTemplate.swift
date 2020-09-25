e0ba0793aaa4361ac4234ca9562606d61f4b0a0f
Роман Юн <romanyun@MacBook-Pro-Roman.local>
2020-09-25 14:07:15 +0900

    Reload files

--- youtube_iOSApp/ConstantsTemplate.swift
+++ youtube_iOSApp/ConstantsTemplate.swift
@@ -15,5 +15,4 @@ struct Constants {
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
 }
- 
 */
